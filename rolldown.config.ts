8a758be2485780859866640f8d274364250a3b64
Kenji Saito <ken-yo@mbr.nifty.com>
2026-08-11 22:34:28 +0900

    first commit

---
+++ rolldown.config.ts
@@ -0,0 +1,19 @@
+import { replacePlugin } from 'rolldown/plugins';
+
+export default {
+  input: 'dist/my-element.js',
+  output: {
+    file: 'dist/my-element.bundled.js',
+    format: 'esm',
+  },
+  onwarn(warning: Record<string, unknown>) {
+    if (warning.code !== 'THIS_IS_UNDEFINED') {
+      console.error(`(!) ${warning.message}`);
+    }
+  },
+  plugins: [
+    replacePlugin({},{
+      preventAssignment: false,
+    }),
+  ],
+};
